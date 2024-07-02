85e675c951301b31a4a265b969690bde5d578ea2
Chrixx190 <86635092+Chrixx190@users.noreply.github.com>
2024-07-02 15:59:40 -0500

    cambios en las credenciales

--- firebase/credenciales.js
+++ firebase/credenciales.js
@@ -4,12 +4,12 @@ import { initializeApp } from "firebase/app";
 //Añadimos las crendeciales de firebase que nos dio la propia plataforma
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqbofH-h-2Nrq4hMja1THVxhH_lvITLYc",
-  authDomain: "plataforma-adopcion-b85d7.firebaseapp.com",
-  projectId: "plataforma-adopcion-b85d7",
-  storageBucket: "plataforma-adopcion-b85d7.appspot.com",
-  messagingSenderId: "455950456206",
-  appId: "1:455950456206:web:5f361fd5a5fd21802280df"
+  apiKey: "#",
+  authDomain: "#",
+  projectId: "#",
+  storageBucket: "#",
+  messagingSenderId: "#",
+  appId: "#"
 };
 
 // Inicializamos y exportamos la configuracion de Firebase
